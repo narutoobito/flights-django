82b7dd6953ff8913dfe69c81afdd0ece36996cbc
Udit Sharma <lunaticfrienge.udit@rediffmail.com>
2020-07-18 01:08:53 +0530

    postgresql trial by denis ivy to heroku

--- airline/urls.py
+++ airline/urls.py
@@ -18,6 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('flights/',include("flights.urls")),
-    path('users/',include("users.urls")),
+    path('flights/', include("flights.urls")),
+    path('users/', include("users.urls")),
 ]
